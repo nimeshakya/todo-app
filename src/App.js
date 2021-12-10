0f2c398fdebf588acb92bb8c0e82ebcccb49caea
nimeshakya <nimeshakyabtw@gmail.com>
2021-12-10 18:42:15 +0545

    project cleared

--- src/App.js
+++ src/App.js
@@ -1,46 +1,7 @@
-import React, { useState, useEffect } from 'react';
-
-import Input from './Components/Input';
-import TaskList from './Components/TaskList';
+import React from 'react';
 
 const App = () => {
-    const [task, setTask] = useState('');
-    const [todos, setTodos] = useState([]);
-    const [editTodo, setEditTodo] = useState(null);
-
-    useEffect(() => {
-        const todosData = localStorage.getItem('todos');
-        if (todosData) {
-            setTodos(JSON.parse(todosData));
-        } else {
-            setTodos([]);
-        }
-    }, []);
-    useEffect(() => {
-        localStorage.setItem('todos', JSON.stringify(todos));
-    }, [todos]);
-
-    return (
-        <main>
-            <h1>To-Do List</h1>
-            <Input
-                task={task}
-                setTask={setTask}
-                todos={todos}
-                setTodos={setTodos}
-                editTodo={editTodo}
-                setEditTodo={setEditTodo}
-            />
-            <TaskList
-                task={task}
-                setTask={setTask}
-                todos={todos}
-                setTodos={setTodos}
-                editTodo={editTodo}
-                setEditTodo={setEditTodo}
-            />
-        </main>
-    );
+    return <div>To Do App</div>;
 };
 
 export default App;
