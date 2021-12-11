21d244060d4d4398d6f9ab5487b1f07da9b33e96
nimeshakya <nimeshakyabtw@gmail.com>
2021-12-11 22:09:07 +0545

    imported useEffect from 'react'

--- src/Context/GlobalState.js
+++ src/Context/GlobalState.js
@@ -1,5 +1,4 @@
-import React, { useReducer } from 'react';
-import { useEffect } from 'react/cjs/react.development';
+import React, { useEffect, useReducer } from 'react';
 
 import AppReducer from './AppReducer';
 
